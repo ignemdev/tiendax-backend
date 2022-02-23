32b94690eba96cd99be3a4c955cd8df83c1a0dbe
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-23 10:59:14 -0400

    valor por default en estado con enumerable estado

--- src/Tiendax.Core/Entities/BaseEntity.cs
+++ src/Tiendax.Core/Entities/BaseEntity.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tiendax.Core.Enumerables;
 
 namespace Tiendax.Core.Entities
 {
     public abstract class BaseEntity
     {
         public int Id { get; set; }
-        public bool? Activo { get; set; }
+        public bool? Activo { get; set; } = Convert.ToBoolean((int)Estado.Activo);
         public DateTime? Creado { get; set; }
         public DateTime? Modificado { get; set; }
     }
