9b8bd7e7a93b0f4ba4e76664d9ca291c7922acc6
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-23 10:58:57 -0400

    enumerable para estados

---
+++ src/Tiendax.Core/Enumerables/Estado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiendax.Core.Enumerables
+{
+    public enum Estado : int
+    {
+        Desactivo,
+        Activo
+    }
+}
