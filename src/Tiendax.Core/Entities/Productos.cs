37c6f4d0e9182591155cb354a117cc32629584e4
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-23 02:18:31 -0400

    Arreglo en campo sku de la entidad producto

--- src/Tiendax.Core/Entities/Productos.cs
+++ src/Tiendax.Core/Entities/Productos.cs
@@ -15,7 +15,7 @@ namespace Tiendax.Core.Entities
 
         public int Id { get; set; }
         public string Nombre { get; set; } = null!;
-        public string? Sku { get; set; }
+        public string Sku { get; set; } = null!;
         public int Stock { get; set; }
         public string Descripcion { get; set; } = null!;
         public int MarcaId { get; set; }
