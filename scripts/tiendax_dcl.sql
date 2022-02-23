1f76ec4d98561e47cde8600d98a597ba8ca9a3b5
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-22 21:36:34 -0400

    script dcl creado para base de datos

---
+++ scripts/tiendax_dcl.sql
@@ -0,0 +1,10 @@
+USE [Tiendax]
+GO
+
+CREATE LOGIN [ignem] WITH PASSWORD = 'clarotest';
+
+CREATE USER [ignem] FOR LOGIN [ignem];
+
+CREATE SCHEMA [mant];
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA :: [mant] TO [ignem];
